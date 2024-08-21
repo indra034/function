46907c8845ba2c3551fe2b698b82de7bfe86a2e8
indra034 <partha.raj5@gmail.com>
2024-08-21 07:34:53 -0400

    add apply discount

---
+++ applyDiscount.js
@@ -0,0 +1,17 @@
+
+function applyDiscount (order) {
+    const discountAmount = order.orderTotal * order.discountPercentage;
+    const grossAmount = order.orderTotal - discountAmount;
+    return {
+        orderTotal: order.orderTotal,
+        discountPercentage: order.discountPercentage,
+        grossAmount: grossAmount,
+    };
+}
+const sampleOrder = {
+    orderTotal: 100,
+    discountPercentage: 0.1
+}
+const updateOrder = applyDiscount(sampleOrder);
+console.log(sampleOrder);
+console.log(updateOrder);
